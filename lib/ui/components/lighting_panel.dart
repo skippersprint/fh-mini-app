606877eaf8bc696279f6dd5a69fe9eab72eebb65
Ankit Jangid <ajangid663@gmail.com>
2022-10-14 20:56:29 +0530

    Color picker experiments

--- lib/ui/components/lighting_panel.dart
+++ lib/ui/components/lighting_panel.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 import '../../utils/widget_functions.dart';
 import 'pod_view.dart';
@@ -16,8 +17,9 @@ class LightingPanel extends StatefulWidget {
 }
 
 class _LightingPanelState extends State<LightingPanel> {
+  Color color = Color.fromARGB(255, 255, 0, 200);
   @override
-   Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Column(
       children: [
@@ -28,8 +30,58 @@ class _LightingPanelState extends State<LightingPanel> {
           size: size,
           isSpin: spinType,
         ),
-        
+        HueRingPicker(
+          pickerAreaBorderRadius: BorderRadius.circular(20),
+            colorPickerHeight: 160,
+            displayThumbColor: false,
+            enableAlpha: false,
+            pickerColor: color,
+            onColorChanged: (color) {
+              this.color = color;
+            })
       ],
+    );
+  }
+
+  pickColor(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+            title: Text('Pick your color'),
+            content: Column(
+              children: [
+                blockColorPicker(),
+                TextButton(
+                  child: Text('SELECT'),
+                  onPressed: (() {
+                    debugPrint('$color');
+                    Navigator.of(context).pop();
+                  }),
+                ),
+              ],
+            )));
+  }
+
+  Widget MaterialColorPicker() {
+    return MaterialPicker(
+      pickerColor: color,
+      onColorChanged: (color) => setState(() => this.color = color),
+    );
+  }
+
+  Widget blockColorPicker() {
+    return BlockPicker(
+      pickerColor: color,
+      onColorChanged: (color) => setState(() => this.color = color),
+    );
+  }
+
+  Widget packageColorPicker() {
+    return HueRingPicker(
+      pickerColor: color,
+      onColorChanged: (color) => setState(() {
+        this.color = color;
+      }),
     );
   }
 }
